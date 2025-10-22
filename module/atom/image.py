6e1416ce5076985af578bab7c37f2e99a2533252
MG <vancii1201@163.com>
2025-10-22 22:14:22 +0800

    调整对弈竞猜图片以及对应参数

--- module/atom/image.py
+++ module/atom/image.py
@@ -355,8 +355,8 @@ if __name__ == "__main__":
     #print(jade.roi_front)
     #print(jade.roi_back)
 
-    from tasks.Component.GeneralInvite.assets import GeneralInviteAssets
-    jade = GeneralInviteAssets.I_I_ACCEPT_JY
+    from tasks.FrogBoss.assets import FrogBossAssets
+    jade = FrogBossAssets.I_FROG_BOSS_REST
     jade.method = 'Template matching'
     detect_image(IMAGE_FILE, jade)
     print(jade.roi_front)
