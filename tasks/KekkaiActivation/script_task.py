9c37f14a662ee5dda9a51408b4b5b87438290b50
MG <vancii1201@163.com>
2025-11-10 16:58:25 +0800

    进入结界逻辑修改

--- tasks/KekkaiActivation/script_task.py
+++ tasks/KekkaiActivation/script_task.py
@@ -22,7 +22,7 @@ from tasks.KekkaiActivation.assets import KekkaiActivationAssets
 from tasks.KekkaiActivation.utils import parse_rule
 from tasks.KekkaiActivation.config import ActivationConfig
 from tasks.Utils.config_enum import ShikigamiClass
-from tasks.GameUi.page import page_main, page_guild
+from tasks.GameUi.page import page_main, page_guild,page_realm
 from tasks.KekkaiActivation.config import CardType
 
 """ 结界挂卡 """
@@ -31,13 +31,13 @@ class ScriptTask(KU, KekkaiActivationAssets):
     def run(self):
         con = self.config.kekkai_activation.activation_config
         self.ui_get_current_page()
-        self.ui_goto(page_guild)
+        self.ui_goto(page_realm)
 
         # 在寮的主界面 检查是否有收取体力或者是收取寮资金
         # self.check_guild_ap_or_assets()
 
         # 进入寮结界
-        self.goto_realm()
+        # self.goto_realm()
 
         if con.exchange_before:
             self.check_max_lv(con.shikigami_class)
@@ -413,9 +413,10 @@ if __name__ == "__main__":
     from module.device.device import Device
     import cv2
 
-    c = Config('switch')
+    c = Config('YZJ-JP')
     d = Device(c)
-
     t = ScriptTask(c, d)
-    t.check_card_num()
+    t.screenshot()
+
+    t.run()
     # t.run_activation(t.config.kekkai_activation.activation_config)
